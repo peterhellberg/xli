b793141993d0c6364d155fabba369397415c5377
Peter Hellberg <peter@c7.se>
2015-08-05 03:49:10 +0200

    Initial command documentation

---
+++ doc.go
@@ -0,0 +1,15 @@
+/*
+
+Minimal version of xli written in Go
+
+Main use case is to allow lynx to open images
+under OS X (and hopefully other operating systems)
+
+Installation:
+		go get -u github.com/peterhellberg/xli
+
+Usage:
+		xli image.jpg
+
+*/
+package main // import "github.com/peterhellberg/xli"
